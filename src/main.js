fb608e30dfa41335fcecc948d62d0d051d692284
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-11-25 08:24:23 +0300

    stops referencing document in section rendering

--- src/main.js
+++ src/main.js
@@ -1,46 +1,46 @@
-import {createSiteRatingTemplate} from './view/site-rating-view.js';
-import {createSiteMenuTemplate} from './view/site-menu-view.js';
-import {createSiteSortTemplate} from './view/site-sort-view.js';
-import {createSiteFilmsTemplate} from './view/site-films-view.js';
-import {createShowMoreTemplate} from './view/site-show-more-view.js';
-import {createSiteAllFilmsTemplate} from './view/site-all-films-list-view.js';
-import {createSiteFilmCardTemplate} from './view/site-film-card-view.js';
-import {createSiteTopRatedFilmsTemplate} from './view/site-top-rated-view.js';
-import {createSiteMostCommentedFilmsTemplate} from './view/site-most-commented-view.js';
-import {createPopupTemplate} from './view/site-popup-view.js';
-import {renderTemplate, RenderPosition} from './render.js';
+import { createSiteRatingTemplate} from './view/site-rating-view.js';
+import { createSiteMenuTemplate} from './view/site-menu-view.js';
+import { createSiteSortTemplate} from './view/site-sort-view.js';
+import { createSiteFilmsTemplate} from './view/site-films-view.js';
+import { createShowMoreTemplate} from './view/site-show-more-view.js';
+import { createSiteAllFilmsTemplate} from './view/site-all-films-list-view.js';
+import { createSiteFilmCardTemplate} from './view/site-film-card-view.js';
+import { createSiteTopRatedFilmsTemplate} from './view/site-top-rated-view.js';
+import { createSiteMostCommentedFilmsTemplate} from './view/site-most-commented-view.js';
+import { createPopupTemplate} from './view/site-popup-view.js';
+import { renderTemplate,  RenderPosition} from './render.js';
 
-const renderBeforeEnd = (container, template)=> renderTemplate(container,template,RenderPosition.BEFOREEND);
+const renderBeforeEnd = (container, template) => renderTemplate(container, template, RenderPosition.BEFOREEND);
 
-const renderFilmItems = (container, count)=>{
-  Array(count).fill(0).forEach(()=> renderBeforeEnd(container, createSiteFilmCardTemplate()));
+const renderFilmItems = (container, count) => {
+  Array(count).fill(0).forEach(() => renderBeforeEnd(container, createSiteFilmCardTemplate()));
 };
-const renderAllFilms = (container)=>{
-  renderBeforeEnd(container,createSiteAllFilmsTemplate());
-  renderFilmItems(document.querySelector('.films-list__container'), 5);
+const renderAllFilms = (container) => {
+  renderBeforeEnd(container, createSiteAllFilmsTemplate());
+  renderFilmItems(container.querySelector('.films-list__container'), 5);
   renderBeforeEnd(container, createShowMoreTemplate());
 };
-const renderTopRated = (container)=>{
+const renderTopRated = (container) => {
   renderBeforeEnd(container, createSiteTopRatedFilmsTemplate());
-  renderFilmItems( document.querySelector('.films-list--top-rated').querySelector('.films-list__container'), 2);
+  renderFilmItems(container.querySelector('.films-list--top-rated').querySelector('.films-list__container'), 2);
 
 };
-const renderMostCommented = (container)=>{
+const renderMostCommented = (container) => {
   renderBeforeEnd(container, createSiteMostCommentedFilmsTemplate());
-  renderFilmItems(document.querySelector('.films-list--most-commented').querySelector('.films-list__container'), 2);
+  renderFilmItems(container.querySelector('.films-list--most-commented').querySelector('.films-list__container'), 2);
 };
 
-const renderFilms = (container)=>{
+const renderFilms = (container) => {
   renderAllFilms(container);
   renderTopRated(container);
   renderMostCommented(container);
 };
 
-const renderSite = (container)=> {
-  renderBeforeEnd(container,createSiteMenuTemplate());
-  renderBeforeEnd(container,createSiteSortTemplate());
-  renderBeforeEnd(container,createSiteFilmsTemplate());
-  renderFilms(document.querySelector('.films'));
+const renderSite = (container) => {
+  renderBeforeEnd(container, createSiteMenuTemplate());
+  renderBeforeEnd(container, createSiteSortTemplate());
+  renderBeforeEnd(container, createSiteFilmsTemplate());
+  renderFilms(container.querySelector('.films'));
 };
 
 renderBeforeEnd(
